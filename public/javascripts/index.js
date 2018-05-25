4859341ce17886c1dd95eea933f513fb6f7547e8
Danteese <dalnte@me.com>
2018-05-24 22:13:36 -0500

    Tablas con contenido colapsables

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $('.datepicker').pickadate({
+   selectMonths: true, // Creates a dropdown to control month
+   selectYears: 2, // Creates a dropdown of 15 years to control year,
+   today: 'Today',
+   clear: 'Clear',
+   close: 'Ok'
+ })
+});
