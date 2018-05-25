c0716a9e9eac477bcf57f33523d76852b4cc74a2
Danteese <dalnte@me.com>
2018-05-24 23:18:32 -0500

    First page finished

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:transactionID', function(req, res){
+  res.render('transaction_details', {tID: transactionID});
+  // TODO: Create the middleware to check in the database for correc id.
+});
+
+
 module.exports = router;
