8ec8c33c6d43c3c9bc487148a062c4577c2ef748
Danteese <dalnte@me.com>
2018-09-17 16:49:07 -0500

    Prot 5

--- public/javascripts/common/masterdb.js
+++ public/javascripts/common/masterdb.js
@@ -9,13 +9,14 @@
 
 // Firefighter
 var config = {
-  apiKey: "AIzaSyALs0CPCUtF8Qz0nZLKm-lGIIRjqVaGYJU",
-  authDomain: "prototipo4-57544.firebaseapp.com",
-  databaseURL: "https://prototipo4-57544.firebaseio.com",
-  projectId: "prototipo4-57544",
-  storageBucket: "prototipo4-57544.appspot.com",
-  messagingSenderId: "889527900945"
+  apiKey: "AIzaSyDikBnf9yHLmGY4jIovJnKGJ7XOfaxI4m4",
+  authDomain: "bucket1-bc82b.firebaseapp.com",
+  databaseURL: "https://bucket1-bc82b.firebaseio.com",
+  projectId: "bucket1-bc82b",
+  storageBucket: "bucket1-bc82b.appspot.com",
+  messagingSenderId: "758565469890"
 };
+
 
 const app = firebase.initializeApp(config);
 const auth = firebase.auth();
